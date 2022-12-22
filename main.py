afe2810a56e2d286cf5921033b941a5b678f2507
unknown <trunovviacheslav@gmail.com>
2022-12-22 14:29:45 +0300

    On branch master 	new file:   .gitignore 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+dic = {'one': 1, 'two': 2, 'three': 3}
+
+while True:
+    your_input = input('Type in word ("one", "two" or "three"): ')
+    if your_input == 'Bye':
+        print('It was fun! Goodbye...')
+        break
+    elif dic.get(your_input) != None:
+        print(f'Good job! Value for this key is: {dic.get(your_input)}')
+    else: print('There is no such key :( Try again!')
